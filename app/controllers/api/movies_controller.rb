00b89d6848d8028d6d488a69d005e7ba8eae2601
dani7beth <danielledona28@gmail.com>
2021-06-18 15:23:18 -0600

    movie controller

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,2 +1,47 @@
 class Api::MoviesController < ApplicationController
+  before_action :set_theater
+  before_action :set_movie, only: [:show, :update, :destroy]
+
+  def index
+    render json: @theater.movies.all
+  end
+
+  def show
+    render json: @movie
+  end
+
+  def create
+    @new_movie = Movie.new(movie_params)
+    if @new_movie.save
+      render json: @new_movie
+    else
+      render json: {errors: @new_movie.errors}, status: 422
+    end
+  end
+
+  def update
+    if @movie.update(movie_params)
+      render json: @movie
+    else
+      render json: {errors: @movie.errors}, status: 422
+    end
+  end
+
+  def destroy
+    @movie.destroy
+    render json: {message: 'Movie deleted'}
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :duration, :rating)
+  end
+
+  def set_movie
+    @movie = @theater.movies.find(params[:id])
+  end
+
+  def set_theater
+    @theater = Theater.find(params[:theater_id])
+  end
 end
--- app/models/theater.rb
+++ app/models/theater.rb
@@ -1,3 +1,3 @@
 class Theater < ApplicationRecord
-  has_many :movies 
+  has_many :movies, dependent: :destroy
 end
