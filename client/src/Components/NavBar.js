a6b4bc2bf93b54a26619f68ae7f27a8507747a5e
dani7beth <danielledona28@gmail.com>
2021-06-18 14:34:12 -0600

    added home, navbar, nomatch

---
+++ client/src/Components/NavBar.js
@@ -0,0 +1,12 @@
+export const NavBar = () => {
+  return (
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/theaters">Theaters</a>
+      </li>
+    </ul>
+  );
+};
---
+++ client/src/Components/NoMatch.js
@@ -0,0 +1,7 @@
+export const NoMatch = () => {
+  return (
+    <>
+      <h1>404 Not Found :(</h1>
+    </>
+  );
+};
