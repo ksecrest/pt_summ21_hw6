fcfd06dbdd6b4befad0dabfba32053893d7cd4e3
dani7beth <danielledona28@gmail.com>
2021-06-18 14:15:48 -0600

    created seeds files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ratings = ['G','PG', 'PG-13', 'R']
+
+10.times do
+  theater = Theater.create(name: Faker::Company.name)
+  20.times do
+    theater.movies.create(title: Faker::Movie.title, duration: rand(1...2).to_s + " hours", rating: ratings.sample)
+  end
+end
